3351b6f654b9f3d6a3532f1f07c9b30e09d71784
Shinmera <shinmera@tymoon.eu>
2016-02-24 11:06:12 +0100

    I'm dumb.

--- event.lisp
+++ event.lisp
@@ -78,7 +78,7 @@
    (cancelled :initform NIL :accessor cancelled :mutable T))
   (:metaclass event-class)
   (:default-initargs
-   :initform *origin*))
+   :origin *origin*))
 
 (defmethod initialize-instance :around ((event event) &key)
   (with-immutable-slots-unlocked ()
